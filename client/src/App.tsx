20c46843f942d9afad064179bee79e203da2c316
ADUDESLAPTOP\ncta2 <nick.nguyen2150@gmail.com>
2026-01-06 23:07:58 +1030

    init frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-500">
+      <h1 className="text-2xl font-bold text-yellow-600">
+        Test
+      </h1>
+    </div>
+  )
+}
